b4836e24b09d46f8b2d600af89d7addda7468d84
jissssoo1717 <jisoolee7101@gmail.com>
2023-11-08 15:15:05 +0900

    Firebase Project

---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCyrQpMrGP_0L4JPuB8_wWeq0DGyZv87so",
+  authDomain: "nwitter-reloaded-55647.firebaseapp.com",
+  projectId: "nwitter-reloaded-55647",
+  storageBucket: "nwitter-reloaded-55647.appspot.com",
+  messagingSenderId: "1094440519616",
+  appId: "1:1094440519616:web:7e4dd85cfbb19bf5216a25",
+};
+
+const app = initializeApp(firebaseConfig);
